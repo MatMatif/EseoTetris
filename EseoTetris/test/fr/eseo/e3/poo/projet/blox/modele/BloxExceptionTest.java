1f9f82fabc088fadb617c4bcd37099000b1f6f8c
mathi <mathieu@flesch.com>
2026-05-20 21:44:06 +0200

    Implémentation des Tests pour BloxException

--- EseoTetris/test/fr/eseo/e3/poo/projet/blox/modele/BloxExceptionTest.java
+++ EseoTetris/test/fr/eseo/e3/poo/projet/blox/modele/BloxExceptionTest.java
@@ -1,14 +1,29 @@
 package fr.eseo.e3.poo.projet.blox.modele;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class BloxExceptionTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+    @Test
+    void testConstructeurEtGettersCollision() {
+        String message = "Collision détectée";
+        int type = BloxException.BLOX_COLLISION;
+        
+        BloxException exception = new BloxException(message, type);
+        
+        assertEquals(message, exception.getMessage(), "Le message de l'exception doit être correct");
+        assertEquals(type, exception.getType(), "Le type de l'exception doit être BLOX_COLLISION");
+    }
+    
+    @Test
+    void testConstructeurEtGettersSortiePuits() {
+        String message = "Sortie du puits";
+        int type = BloxException.BLOX_SORTIE_PUITS;
+        
+        BloxException exception = new BloxException(message, type);
+        
+        assertEquals(message, exception.getMessage(), "Le message de l'exception doit être correct");
+        assertEquals(type, exception.getType(), "Le type de l'exception doit être BLOX_SORTIE_PUITS");
+    }
 }
